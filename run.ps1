8257756a5587fbae924560358da0aeda5d40f734
Kyle Kotowick <kotowick@invictonlabs.com>
2022-03-03 19:05:29 -0500

    Fix for catching error from command file execution

--- run.ps1
+++ run.ps1
@@ -17,22 +17,33 @@ $_cmdfile = "$_path/cmd.$_id.ps1"
 Write-Output '$ErrorActionPreference = "Stop"' | Out-File -FilePath "$_cmdfile"
 # Now write the command itself 
 Write-Output "$_cmd" | Out-File -Append -FilePath "$_cmdfile"
+# Always force the command file to exit with the last exit code
+Write-Output 'Exit $LASTEXITCODE' | Out-File -Append -FilePath "$_cmdfile"
 
 # Equivalent of set +e
 $ErrorActionPreference = "Continue"
 $_process = Start-Process powershell.exe -ArgumentList "-file ""$_cmdfile""" -Wait -PassThru -NoNewWindow -RedirectStandardError "$_stderrfile" -RedirectStandardOutput "$_stdoutfile"
 $_exitcode = $_process.ExitCode
+Write-Output $_process.ExitCode | Out-File -FilePath "exitfile.txt"
 $ErrorActionPreference = "Stop"
 
 # Delete the command file
 Remove-Item "$_cmdfile"
 
-[System.IO.File]::WriteAllText("$_exitcodefile", "$_exitcode", [System.Text.Encoding]::ASCII)
-
 if (( "$_failonerr" -eq "true" ) -and $_exitcode) {
     # If it should fail on an error, and it did fail, read the stderr file
     # Exit with the error message and code
     $_stderr = [System.IO.File]::ReadAllText("$_stderrfile")
-    Write-Error "$_stderr"
+
+    # Since we're exiting with an error code, we don't need to read the output files in the Terraform config,
+    # and we won't get a chance to delete them via Terraform, so delete them now
+    Remove-Item "$_stderrfile" -ErrorAction Ignore
+    Remove-Item "$_stdoutfile" -ErrorAction Ignore
+
+    if ("$_stderr") {
+        Write-Error "$_stderr"
+    }
     exit $_exitcode
 }
+
+[System.IO.File]::WriteAllText("$_exitcodefile", "$_exitcode", [System.Text.Encoding]::ASCII)
