031f9353fb9b946864f4657e576d272ce72ada4d
KyleKotowick <kotowick@invictonlabs.com>
2023-03-18 10:43:27 -0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,7 +1,8 @@
 terraform {
   // 0.15.0 - 0.15.3 had a bug where it threw an error if an output
   // was marked as sensitive.
-  required_version = ">= 0.13, !=0.15.0, !=0.15.1, !=0.15.2, !=0.15.3"
+  // 0.13.0 and 1.3.0 have bugs where it doesn't handle output correctly
+  required_version = ">= 0.13.1, !=0.15.0, !=0.15.1, !=0.15.2, !=0.15.3, !=1.3.0"
   required_providers {
     null = {
       source  = "hashicorp/null"
